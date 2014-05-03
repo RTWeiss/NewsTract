6b91e293984f42165bd7bd8c3a139fff1543ef13
stevenmiller888 <stevenmiller888@me.com>
2014-05-03 09:41:28 -0700

    First commit on the NewsTract app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsTract::Application.config.secret_key_base = 'd4a11ffa1870f9ee569741db0fa96762996eca25a5394b7430dfd9fbdae326a4c6db5a7816c10d6228cdfc0bf52d6a36a819c5b8de9744ed1d24bc6a59d0b7b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewsTract::Application.config.session_store :cookie_store, key: '_NewsTract_session'
