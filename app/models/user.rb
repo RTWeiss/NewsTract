fb0fe3b564ae946720d63d78f2eb8422dc63dfac
stevenmiller888 <stevenmiller888@me.com>
2014-05-03 13:04:06 -0700

    Added has_secure_password to User model. So, the seeds now work and rspec tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+  # Adds methods to set and authenticate against a BCrypt password,
+  # Adds adds validations for presence of password on create and confirmation of password 
+  has_secure_password
+  
 end
