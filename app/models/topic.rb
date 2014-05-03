8253ffc17a93d7cfa5e6d014d20d90fadc85168b
stevenmiller888 <stevenmiller888@me.com>
2014-05-03 13:24:37 -0700

    Added has_many, has_many through:, and belongs_to relationships

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :users, through: :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :topics, through: :subscriptions
 
   # Adds methods to set and authenticate against a BCrypt password,
   # Adds adds validations for presence of password on create and confirmation of password 
