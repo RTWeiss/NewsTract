2048b92e3435266c70bebee2fde4626be3872a83
stevenmiller888 <stevenmiller888@me.com>
2014-05-06 11:06:56 -0700

    User will now only see the articles for topics he/she is subscribed to, also refactored the parsing and summarization code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,27 @@
 class ArticlesController < ApplicationController
 
-  def index
-    @articles = Article.all
-  end
-
   def create
     # Store the topic urls in strings
     #Topic_id = 1
-    world_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=w&output=rss"
+    world_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=w&output=rss"
     #Topic_id = 2
-    us_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=n&output=rss"
+    us_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=n&output=rss"
     #Topic_id = 3
-    business_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=b&output=rss"
+    business_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=b&output=rss"
     #Topic_id = 4
-    technology_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=tc&output=rss"
+    technology_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=tc&output=rss"
     #Topic_id = 5
-    entertainment_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=e&output=rss"
+    entertainment_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=e&output=rss"
     #Topic_id = 6
-    sports_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=s&output=rss"
+    sports_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=s&output=rss"
     #Topic_id = 7
-    science_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=snc&output=rss"
+    science_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=snc&output=rss"
     #Topic_id = 8
-    health_url_string = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=m&output=rss"
+    health_url = "https://news.google.com/news/section?pz=1&ned=us&hl=en&topic=m&output=rss"
     
     #Add the url strings to an array
     url_array = []
-    url_array.push world_url_string
-    url_array.push us_url_string
-    url_array.push business_url_string
-    url_array.push technology_url_string
-    url_array.push entertainment_url_string
-    url_array.push sports_url_string
-    url_array.push science_url_string
-    url_array.push health_url_string
+    url_array.push(world_url, us_url, business_url, technology_url, entertainment_url, sports_url, science_url, health_url)
 
     url_array.each do |url|
       # Retrieve and parse the url
@@ -52,13 +41,12 @@ class ArticlesController < ApplicationController
       # Use Sumitup to generate a summary of the article's body
       summary = parser.summarize(original_doc.body)
       # Create the article with the title, summary, and topic_id, which is the element's index plus 1
-      Article.create(title: title, summary: summary, topic_id: url_array.index(url) + 1)
-      #get url index
+      article = Article.find_by_topic_id(url_array.index(url) + 1)
+      article.title = title
+      article.summary = summary
+      article.save
     end
 
-    # #Add MetaInspector to grab images from original article
-    # page = MetaInspector.new(main_article_link)
-    # image_link = page.image
   end
 
 end
