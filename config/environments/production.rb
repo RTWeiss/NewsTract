d1202b991bba2b3c4771993f746b77fab6fb8ab3
stevenmiller888 <stevenmiller888@me.com>
2014-05-07 21:05:58 -0700

    removed devise mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,16 +80,4 @@ NewsTract::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://newstract.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp 
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
-
 end
