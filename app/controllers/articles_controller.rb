a4b87a988e50f1213a2b177dc62cfcb6883c5585
stevenmiller888 <stevenmiller888@me.com>
2014-05-13 22:28:16 -0700

    commented out search functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
 
-  def search
-    @search = SimpleSearch.new SimpleSearch.get_params(params)
-    if @search.valid?
-      @articles = @search.search_within Article.all, :title
-    end
-  end
+  # def search
+  #   @search = SimpleSearch.new SimpleSearch.get_params(params)
+  #   if @search.valid?
+  #     @articles = @search.search_within Article.all, :title
+  #   end
+  # end
 
 end
--- app/models/simple_search.rb
+++ app/models/simple_search.rb
@@ -1,30 +1,30 @@
-class SimpleSearch
-  include ActiveModel::Model
+# class SimpleSearch
+#   include ActiveModel::Model
 
-  #Generic Search 'q'
-  attr_accessor :q
-  validates_length_of :q, minimum: 1, message: 'Please enter at least 1 letter to search'
+#   #Generic Search 'q'
+#   attr_accessor :q
+#   validates_length_of :q, minimum: 1, message: 'Please enter at least 1 letter to search'
 
-  # Add your other search params or validations here
+#   # Add your other search params or validations here
 
-  def persisted?
-    false
-  end
+#   def persisted?
+#     false
+#   end
 
-  def search_within scope, column_name
-    #example where scope: only added when the q param is present
-    unless self.q.blank?
-      # SQL Injection protection example with LIKE operator
-      # Which matches partial strings:
-      scope = scope.where("#{column_name} ILIKE ('%' || ? || '%')", self.q)
-    end
-    #Add your own filters here
-    scope
-  end
+#   def search_within scope, column_name
+#     #example where scope: only added when the q param is present
+#     unless self.q.blank?
+#       # SQL Injection protection example with LIKE operator
+#       # Which matches partial strings:
+#       scope = scope.where("#{column_name} ILIKE ('%' || ? || '%')", self.q)
+#     end
+#     #Add your own filters here
+#     scope
+#   end
 
-  def self.get_params(params, as = :simple_search)
-    params.require(as).permit(:q) if params.has_key? as
-  end
-end
+#   def self.get_params(params, as = :simple_search)
+#     params.require(as).permit(:q) if params.has_key? as
+#   end
+# end
 
-# E.g. http://localhost:3000/articles/search?simple_search[q]=Appl
+# # E.g. http://localhost:3000/articles/search?simple_search[q]=Appl
